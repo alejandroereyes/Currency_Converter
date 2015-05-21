85edd0c12db8007b1e507a7e1608edbea4de13ce
Alejandro Reyes <alejandroereyes@gmail.com>
2015-05-21 14:45:43 -0500

    added test for diff instances w/diff values should not be equal, passing test

--- spec/Currency_Test_class.rb
+++ spec/Currency_Test_class.rb
@@ -18,10 +18,17 @@ class Currency_Test < Minitest::Test
    assert_equal(100_000_000, @currency.amount)
   end
 
-  def test_diff_instances_of_Currency
+  def test_diff_instances_of_Currency_with_same_values
    setup_Currency_class # Given
    new_currency = Currency.new("USD", 100_000_000) # When
    assert_equal(new_currency.code,@currency.code) # Then
    assert_equal(new_currency.amount, @currency.amount)
+  end
+
+  def test_diff_instances_of_Currency_with_diff_values
+    setup_Currency_class
+    new_currency = Currency.new("NIO", 1_000)
+    refute_equal(new_currency.code, @currency.code)
+    refute_equal(new_currency.amount, @currency.amount)
   end
 end # class
