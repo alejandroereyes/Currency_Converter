1d81b280daf0273650beaaaa9a5082553224d460
Alejandro Reyes <alejandroereyes@gmail.com>
2015-05-21 16:42:04 -0500

    added rates hash to converter & test to make sure has w/rates is readable

--- lib/Currency_converter_class.rb
+++ lib/Currency_converter_class.rb
@@ -1,2 +1,9 @@
 class CurrencyConverter
+  attr_reader :rates
+
+  def initialize
+    @rates = {"USD" => 1.00, "EUR"=> 0.89958, "GBP"=> 0.63840,
+              "INR"=> 63.5795, "AUD"=> 1.26606, "CAD"=> 1.21981,
+              "ZAR"=> 11.8514, "NZD"=> 1.36116, "JPY"=> 121.026}
+  end
 end
--- spec/Currency_converter_test_class.rb
+++ spec/Currency_converter_test_class.rb
@@ -1,5 +1,16 @@
 require 'minitest/autorun'
-require_relative '..lib/Currency_converter_class.rb'
+require_relative '../lib/Currency_converter_class.rb'
 
 class TestCurrencyConverter < Minitest::Test
+
+  def setup_converter
+    converter = CurrencyConverter.new
+  end
+
+  def test_converter_has_rates
+    setup_converter
+    converter = CurrencyConverter.new
+    conversion_rates = {"USD" => 1.00, "EUR"=> 0.89958, "GBP"=> 0.63840,"INR"=> 63.5795, "AUD"=> 1.26606, "CAD"=> 1.21981,"ZAR"=> 11.8514, "NZD"=> 1.36116, "JPY"=> 121.026}
+    assert_equal(conversion_rates, converter.rates)
+  end
 end
