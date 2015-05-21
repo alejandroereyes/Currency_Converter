83a309c63a40d4afc17426c9a8112a9f1a041ee4
Alejandro Reyes <alejandroereyes@gmail.com>
2015-05-21 13:38:37 -0500

    added test for code & amount, test passing

--- lib/Currency_class.rb
+++ lib/Currency_class.rb
@@ -1,2 +1,8 @@
 class Currency
+  attr_reader :code, :amount
+
+  def initialize(code, amount)
+    @code = code
+    @amount = amount
+  end
 end
--- spec/Currency_Test_class.rb
+++ spec/Currency_Test_class.rb
@@ -4,11 +4,17 @@ require_relative '../lib/Currency_class.rb'
 class Currency_Test < Minitest::Test
 
   def setup_Currency_class
-    @currency = Currency.new
+    @currency = Currency.new("USD", 100_000_00)
   end
 
   def test_Currency_class_exists
     setup_Currency_class # Given
     assert(@currency) # Then
+  end
+
+  def test_currency_code_and_amount
+   setup_Currency_class # Given
+   assert_equal("USD", @currency.code) # Then
+   assert_equal(100_000_00, @currency.amount)
   end
 end # class
