ade2dfaae401c4a12165f57eadb42cefa7a0b5a3
Alejandro Reyes <alejandroereyes@gmail.com>
2015-05-22 12:12:31 -0500

    broke out converter test into 3, test returns same item if same requested, test returns accurate diff currency when requested, test for error being raised when unknown code entered, passing all tests

--- spec/Currency_converter_test_class.rb
+++ spec/Currency_converter_test_class.rb
@@ -23,19 +23,28 @@ class TestCurrencyConverter < Minitest::Test
     assert_equal(0.70, converter.convert_math("EUR", 1.00, "GBP"))
   end
 
-  def test_converter_method
+  def test_converter_method_returns_equal_currency_when_rerquested
     setup_converter
-    error = Unknown_Currency_Code_Error.new
     us_money = Currency.new("USD", 1_000)
     also_us_money = @converter.convert(us_money, "USD")
+    assert_equal(us_money, also_us_money)
+  end
+
+  def test_converter_returns_correct_diff_currency
+    setup_converter
+    us_money = Currency.new("USD", 1_000)
     uk_money = @converter.convert(us_money, "GBP")
     yen_money = @converter.convert(uk_money, "JPY")
-    assert_equal(us_money, also_us_money)
     assert_equal(Currency, uk_money.class)
     assert_equal("GBP", uk_money.code)
     assert_equal(638.40, uk_money.amount)
-    assert_raises(Unknown_Currency_Code_Error){ @converter.convert(us_money, "OOP") }
-    assert_equal("JPY", yen_money.code)
     assert_equal(121_026, yen_money.amount)
+    assert_equal("JPY", yen_money.code)
+  end
+
+  def test_converter_raises_error_with_unknown_currency_code
+    setup_converter
+    us_money = Currency.new("USD", 1_000)
+    assert_raises(Unknown_Currency_Code_Error){ @converter.convert(us_money, "OOP") }
   end
 end
