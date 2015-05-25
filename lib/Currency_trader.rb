746a0b2b6c591c002b14f54338f87a4224b2a0ea
Alejandro Reyes <alejandroereyes@gmail.com>
2015-05-25 17:51:15 -0500

    trader now able to initialize w/multiple converter objects, passing test

--- lib/Currency_trader.rb
+++ lib/Currency_trader.rb
@@ -2,12 +2,18 @@ require_relative 'Currency_class'
 require_relative 'Currency_converter_class'
 
 class Currency_Trader
-  attr_reader :currency, :converter_one, :converter_two
-  def initialize(code = 'USD', amount = 100_000_000, converter_two_rates = {"USD" => 1.00, "EUR"=> 0.89958, "GBP"=> 0.31920,
-                                                                                                "INR"=> 63.5795, "AUD"=> 1.26606, "CAD"=> 1.21981,
-                                                                                                "ZAR"=> 11.8514, "NZD"=> 1.36116, "JPY"=> 121.026})
+#   attr_reader :currency, :converter_one, :converter_two
+#   def initialize(code = 'USD', amount = 100_000_000, converter_two_rates = {"USD" => 1.00, "EUR"=> 0.89958, "GBP"=> 0.31920,
+#                                                                             "INR"=> 63.5795, "AUD"=> 1.26606, "CAD"=> 1.21981,
+#                                                                             "ZAR"=> 11.8514, "NZD"=> 1.36116, "JPY"=> 121.026})
+#     @currency = Currency.new(code, amount)
+#     @converter_one = CurrencyConverter.new
+#     @converter_two = CurrencyConverter.new(converter_two_rates)
+#   end
+# end # class
+  attr_reader :currency, :converters
+  def initialize(code = 'USD', amount = '100_000_000', *converters_array)
     @currency = Currency.new(code, amount)
-    @converter_one = CurrencyConverter.new
-    @converter_two = CurrencyConverter.new(converter_two_rates)
-  end
+    @converters = converters_array
+  end # initialize
 end # class
--- spec/Currency_Trader_Test.rb
+++ spec/Currency_Trader_Test.rb
@@ -4,7 +4,10 @@ require_relative '../lib/Currency_trader.rb'
 class Currency_Trader_Test < Minitest::Test
 
   def setup_Trader_Test
-    @trader = Currency_Trader.new
+    converter_one = CurrencyConverter.new
+    converter_two = CurrencyConverter.new({'USD'=> 1.00, 'GBP'=> 0.31920, 'JPY'=> 121.026})
+    converter_three = CurrencyConverter.new({'USD'=> 1.00, 'GBP'=> 0.31920, 'JPY'=> 121.026})
+    @trader = Currency_Trader.new('USD', 100_000_000, converter_one, converter_two, converter_three)
   end
 
   def test_Currency_Trader_exists
@@ -12,10 +15,20 @@ class Currency_Trader_Test < Minitest::Test
     assert(@trader)
   end
 
-  def test_trader_can_double_money
+  # def test_trader_can_double_money
+  #   setup_Trader_Test
+  #   uk_money = @trader.converter_one.convert(@trader.currency, 'GBP')
+  #   us_money = @trader.converter_two.convert(uk_money, 'USD')
+  #   assert_equal(200_000_000, us_money.amount)
+  # end
+
+  def test_converters_array
     setup_Trader_Test
-    uk_money = @trader.converter_one.convert(@trader.currency, 'GBP')
-    us_money = @trader.converter_two.convert(uk_money, 'USD')
+    uk_money = @trader.converters[0].convert(@trader.currency, 'GBP')
+    yen_money = @trader.converters[1].convert(uk_money, 'JPY')
+    us_money = @trader.converters[2].convert(yen_money, 'USD')
+    assert_equal('GBP', uk_money.code)
+    assert_equal('JPY', yen_money.code)
     assert_equal(200_000_000, us_money.amount)
   end
 end # class
